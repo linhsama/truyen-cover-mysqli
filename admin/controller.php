3f63c1f5008742aa1325983793688c32e04dd196
linhsama <fdcvlinh6@gmail.com>
2022-10-08 03:03:47 +0700

    a

--- admin/controller.php
+++ admin/controller.php
@@ -2,83 +2,83 @@
     if(isset($_GET['direction'])){
     switch($_GET['direction']){
         case "dashboard":
-            require __DIR__."./dashboard/index.php";
+            require __DIR__."/dashboard/index.php";
             break;
        
         case "tai-khoan":
-            require __DIR__."./tai-khoan/index.php";
+            require __DIR__."/tai-khoan/index.php";
             break;
         case "them-tai-khoan":
-            require __DIR__."./tai-khoan/them.php";
+            require __DIR__."/tai-khoan/them.php";
             break;
         case "sua-tai-khoan":
-            require __DIR__."./tai-khoan/sua.php";
+            require __DIR__."/tai-khoan/sua.php";
             break;
         case "xoa-tai-khoan":
-            require __DIR__."./tai-khoan/xoa.php";
+            require __DIR__."/tai-khoan/xoa.php";
             break;
 
 
         case "the-loai":
-            require __DIR__."./the-loai/index.php";
+            require __DIR__."/the-loai/index.php";
             break;
         case "them-the-loai":
-            require __DIR__."./the-loai/them.php";
+            require __DIR__."/the-loai/them.php";
             break;
         case "sua-the-loai":
-            require __DIR__."./the-loai/sua.php";
+            require __DIR__."/the-loai/sua.php";
             break;
         case "xoa-the-loai":
-            require __DIR__."./the-loai/xoa.php";
+            require __DIR__."/the-loai/xoa.php";
             break;
        
         case "truyen-tranh":
-            require __DIR__."./truyen-tranh/index.php";
+            require __DIR__."/truyen-tranh/index.php";
             break;
         case "them-truyen-tranh":
-            require __DIR__."./truyen-tranh/them.php";
+            require __DIR__."/truyen-tranh/them.php";
             break;
         case "sua-truyen-tranh":
-            require __DIR__."./truyen-tranh/sua.php";
+            require __DIR__."/truyen-tranh/sua.php";
             break;
         case "xoa-truyen-tranh":
-            require __DIR__."./truyen-tranh/xoa.php";
+            require __DIR__."/truyen-tranh/xoa.php";
             break;
 
         case "truyen-the-loai":
-            require __DIR__."./truyen-tranh/truyen-the-loai.php";
+            require __DIR__."/truyen-tranh/truyen-the-loai.php";
             break;
         case "xu-ly-truyen-the-loai":
-            require __DIR__."./truyen-tranh/xu-ly-truyen-the-loai.php";
+            require __DIR__."/truyen-tranh/xu-ly-truyen-the-loai.php";
             break;
 
         case "chapter":
-            require __DIR__."./chapter/index.php";
+            require __DIR__."/chapter/index.php";
             break;
         case "them-chapter":
-            require __DIR__."./chapter/them.php";
+            require __DIR__."/chapter/them.php";
             break;
         case "sua-chapter":
-            require __DIR__."./chapter/sua.php";
+            require __DIR__."/chapter/sua.php";
             break;
         case "xoa-chapter":
-            require __DIR__."./chapter/xoa.php";
+            require __DIR__."/chapter/xoa.php";
             break;
        
         case "chapter-noi-dung":
-            require __DIR__."./chapter-noi-dung/index.php";
+            require __DIR__."/chapter-noi-dung/index.php";
             break;
         case "them-chapter-noi-dung":
-            require __DIR__."./chapter-noi-dung/them.php";
+            require __DIR__."/chapter-noi-dung/them.php";
             break;
         case "sua-chapter-noi-dung":
-            require __DIR__."./chapter-noi-dung/sua.php";
+            require __DIR__."/chapter-noi-dung/sua.php";
             break;
         case "xoa-chapter-noi-dung":
-            require __DIR__."./chapter-noi-dung/xoa.php";
+            require __DIR__."/chapter-noi-dung/xoa.php";
             break;
         case "xu-ly-upload":
-            require __DIR__."./chapter-noi-dung/xu-ly-upload.php";
+            require __DIR__."/chapter-noi-dung/xu-ly-upload.php";
                 break;
         default:
             break;
