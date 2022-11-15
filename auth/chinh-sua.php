5917d07e89652d90e61ca03ffe9d1dc711886b55
linhsama <fdcvlinh6@gmail.com>
2022-11-15 23:59:06 +0700

    fix

--- auth/chinh-sua.php
+++ auth/chinh-sua.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chỉnh sửa</title>
+    <title>Truyện Cover</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
--- auth/dang-nhap.php
+++ auth/dang-nhap.php
@@ -10,7 +10,7 @@ if(isset($_SESSION['user'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng nhập</title>
+    <title>Truyện Cover</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
