e66bd7abedae00ca5365e2fb72043ae643ca0987
linhsama <fdcvlinh6@gmail.com>
2022-11-19 17:42:31 +0700

    fix

--- index.php
+++ index.php
@@ -35,22 +35,4 @@
     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v15.0"
         nonce="jB1NhBGh"></script>
 
-    <!-- vchat plugin -->
-    <script lang="javascript">
-    var __vnp = {
-        code: 17256,
-        key: '',
-        secret: '3fe6709227ffc978238ab858c433ec09'
-    };
-    (function() {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.defer = true;
-        ga.src = '//core.vchat.vn/code/tracking.js';
-        var s = document.getElementsByTagName('script');
-        s[0].parentNode.insertBefore(ga, s[0]);
-    })();
-    </script>
-
 </html>
